c5dbf8381a048e55ceee67cb6c828d879ce07a08
Chris Stoddart <cstoddart@rocketmail.com>
2019-03-03 09:59:37 -0600

    Delete keys.js

--- src/keys.js
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_API_KEY = 'AIzaSyDxOUF532KYiskjYp1qsjl02XqQvcD5-s8';
-export const STRIPE_PUBLIC_KEY = 'pk_test_Q6QKPS2TAmgWh3RBhqL7WASO';
-export const FIREBASE_FUNCTION_URL = 'https://us-central1-leeif-222702.cloudfunctions.net/stripe';
-export const ZILLOW_API_KEY = 'X1-ZWz1gryvl6hcsr_4wzn5';
